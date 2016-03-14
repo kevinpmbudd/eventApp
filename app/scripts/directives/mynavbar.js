7e6ae5e9e83075804eded9d05f24c2dc32c5780b
Kevin Budd <kevin@kevinbudd.com>
2016-03-14 14:32:53 -0500

    Chore: Refactor mynavbar to use an inhouse controller
    Replace seperate login controller with a function object inside myNavbar directive

--- app/scripts/directives/mynavbar.js
+++ app/scripts/directives/mynavbar.js
@@ -7,9 +7,32 @@
  * # myNavbar
  */
 angular.module('eventAppApp')
-  .directive('myNavbar', function () {
-    return {
+  .directive('myNavbar', myNavbar);
+
+  function myNavbar() {
+  	var directive = {
+  		restrict: 'E',
       templateUrl: 'views/mynavbar.html',
-      restrict: 'E'
-    };
-  });
+      controller: myNavbarCtrl
+  	};
+
+  	return directive;
+
+  	function myNavbarCtrl($scope, Auth, Ref, $location) {
+      $scope.passwordLogin = function(email, pass) {
+        $scope.err = null;
+        Auth.$authWithPassword({ email: email, password: pass }, { rememberMe: true }).then(
+          redirect, showError
+        );
+      };
+      $scope.logout = function() { Auth.$unauth(); };
+
+      function redirect() {
+        $location.path('/chat');
+      }
+
+      function showError(err) {
+        $scope.err = err;
+      }
+    }
+  }
