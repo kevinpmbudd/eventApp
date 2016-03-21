fc4a83b877dbf019d1fd2519596ebb371da60171
Kevin Budd <kevin@kevinbudd.com>
2016-03-21 13:15:59 -0500

    Chore: remove mynewevent directive and view

--- app/scripts/controllers/newevent.js
+++ app/scripts/controllers/newevent.js
@@ -8,11 +8,38 @@
  * Controller of the eventAppApp
  */
 angular.module('eventAppApp')
-  .controller('NeweventCtrl', function ($scope, Ref, $firebaseArray, $timeout, $location) {
+  .controller('NeweventCtrl', function ($scope, Ref, $firebaseArray, $timeout, $location, uiGmapGoogleMapApi) {
   	$scope.events = $firebaseArray(Ref.child('events'));
     $scope.users = $firebaseArray(Ref.child('users'));
     $scope.guestList = [];
+    // map searchbox
+    $scope.place = {};
+    $scope.showPlaceDetails = function(param) {
+	    $scope.place = param;
+	  }
+	  uiGmapGoogleMapApi.then(function(maps) {
+	  	maps.visualRefresh = true;
+	    // $scope.defaultBounds = new google.maps.LatLngBounds(
+	    //   new google.maps.LatLng(40.82148, -73.66450),
+	    //   new google.maps.LatLng(40.66541, -74.31715));
 
+	    $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 3 };
+
+	    // $scope.map.bounds = {
+	    //   northeast: {
+	    //     latitude:$scope.defaultBounds.getNorthEast().lat(),
+	    //     longitude:$scope.defaultBounds.getNorthEast().lng()
+	    //   },
+	    //   southwest: {
+	    //     latitude:$scope.defaultBounds.getSouthWest().lat(),
+	    //     longitude:-$scope.defaultBounds.getSouthWest().lng()
+	    //   }
+	    // }
+	    // $scope.searchbox.options.bounds = new google.maps.LatLngBounds($scope.defaultBounds.getNorthEast(), $scope.defaultBounds.getSouthWest());
+	  });
+
+
+    // Autocomplete searchbox to add guests to guest list
     $scope.localSearch = function(str) {
       var matches = [];
       $scope.users.forEach(function(person) {
@@ -25,11 +52,6 @@ angular.module('eventAppApp')
       });
       return matches;
     };
-
-    // $scope.addGuest = function(guest) {
-    // 	if(guest.length !== 0)
-    //   	$scope.guestList.push(guest);
-    // };
 
     $scope.selectedPerson = function(guest) {
     	console.log(guest);
@@ -46,24 +68,85 @@ angular.module('eventAppApp')
       }
     };
 
-    $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
-    $scope.options = {scrollwheel: false};
-    var events = {
-      places_changed: function (searchBox) {
+    // $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
+    // $scope.options = {scrollwheel: false};
+    // var events = {
+    //   places_changed: function (searchBox) {
 
-      }
-    }
+    //   }
+    // }
+    // $scope.searchbox = {
+    //   template:'searchbox.tpl.html',
+    //   options: {
+    //     autocomplete: true,
+    //     types: ['establishment']
+    //   },
+    //   events:events,
+    //   parentdiv: 'eventLocation'
+    //   // position: 'top-left'
+
+    // };
     $scope.searchbox = {
       template:'searchbox.tpl.html',
       options: {
+        bounds: {},
         autocomplete: true,
         types: ['establishment']
       },
-      events:events,
-      parentdiv: 'eventLocation'
-      // position: 'top-left'
+      parentdiv:'eventLocation',
+      events: {
+        places_changed: function (searchBox) {
 
-    };
+          // $scope.places = searchBox.getPlaces()
+
+          // if (places.length == 0) {
+          //   return;
+          // }
+          // // For each place, get the icon, place name, and location.
+          // newMarkers = [];
+          // var bounds = new google.maps.LatLngBounds();
+          // for (var i = 0, place; place = $scope.places[i]; i++) {
+          //   // Create a marker for each place.
+          //   var marker = {
+          //     idKey:i,
+          //     place_id: place.place_id,
+          //     name: place.name,
+          //     latitude: place.geometry.location.lat(),
+          //     longitude: place.geometry.location.lng(),
+          //     templateurl:'window.tpl.html',
+          //     templateparameter: place,
+          //     events: {
+          //       click: function (marker) {
+          //         $scope.window.coords = {
+          //           latitude: marker.model.latitude,
+          //           longitude: marker.model.longitude
+          //         }
+          //         $scope.window.templateparameter = marker.model.templateparameter;
+          //         $scope.window.show = true;
+
+          //       }
+          //     }
+          //   };
+          //   newMarkers.push(marker);
+
+          //   bounds.extend(place.geometry.location);
+          // }
+
+          // $scope.map.bounds = {
+          //   northeast: {
+          //     latitude: bounds.getNorthEast().lat(),
+          //     longitude: bounds.getNorthEast().lng()
+          //   },
+          //   southwest: {
+          //     latitude: bounds.getSouthWest().lat(),
+          //     longitude: bounds.getSouthWest().lng()
+          //   }
+          // }
+
+          // $scope.map.markers = newMarkers;
+        }
+      }
+    }
 
   	$scope.addEvent = function(name, host, location) {
   		if( name && host && location ) {
