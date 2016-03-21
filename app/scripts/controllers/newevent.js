4cac00e36ee114a405da929e9f93d49e64f50b9b
Kevin Budd <kevin@kevinbudd.com>
2016-03-20 21:03:40 -0500

    Style: move places search box on new event page off map

--- app/scripts/controllers/newevent.js
+++ app/scripts/controllers/newevent.js
@@ -42,17 +42,20 @@ angular.module('eventAppApp')
     $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
     $scope.options = {scrollwheel: false};
     var events = {
-      places_changed: function (autocomplete) {
+      places_changed: function (searchBox) {
 
       }
     }
     $scope.searchbox = {
       template:'searchbox.tpl.html',
       options: {
-        autocomplete: true
+        autocomplete: true,
+        types: ['establishment']
       },
       events:events,
-      position: 'top-left'
+      parentdiv: 'eventLocation'
+      // position: 'top-left'
+
     };
 
   	$scope.addEvent = function(name, host, location) {
