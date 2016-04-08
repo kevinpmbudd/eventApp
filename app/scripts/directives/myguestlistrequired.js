2c7f9f920bdc6464b2e1feaadf98831a2be1da61
Kevin Budd <kevin@kevinbudd.com>
2016-04-08 15:00:51 -0500

    Feat: validate add guest input

--- app/scripts/directives/myguestlistrequired.js
+++ app/scripts/directives/myguestlistrequired.js
@@ -13,38 +13,11 @@ angular.module('eventAppApp')
       restrict: 'A',
       require: 'ngModel',
       link: function(scope, element, attrs, ctrl) {
-
-        var checkForGuests = function(value) {
-          var contains;
-          if(scope.newEvent.guests.length > 0) {
-            contains = true;
-          }
-          else {
-            contains = false;
-          }
-
-          return contains;
-        };
-
         ctrl.$setValidity('hasGuests', false);
 
-        // $('.new-guest-btn').on('click', function() {
-        //   console.log(element, scope, attrs, ctrl);
-        //   if (scope.newEvent.guest.length !== 0) {
-        //     ctrl.$setValidity('hasGuests', true);
-        //   }
-        // });
-        ctrl.$parsers.unshift(function(viewValue) {
-          var haveGuests = checkForGuests(viewValue);
-
-          ctrl.$setValidity('hasGuests', haveGuests);
-
-          if(haveGuests) {
-            return viewValue;
-          } else {
-            return undefined;
-          }
-        });
+        ctrl.$formatters.unshift(function(value) {
+          ctrl.$setValidity('hasGuests', scope.newEvent.guests.length !== 0);
+        })
       }
     };
   });
